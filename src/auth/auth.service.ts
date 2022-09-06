6dd1a7edf63d04b655fb6290897e15219487c78b
Felipe Jardim <jardim.felipe [at] hotmail.com>
2022-09-06 15:41:51 -0300

    feat: username unique

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -17,6 +21,15 @@ export class AuthService {
     const { username, password } = authCredentialsDto;
 
     const user = this.usersRepository.create({ username, password });
-    await this.usersRepository.save(user);
+
+    try {
+      await this.usersRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
